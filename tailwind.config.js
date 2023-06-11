15d146f101c05e83516fafe61762aca076c7d617
jdpena28 <johnhenrich1128@gmail.com>
2023-06-11 10:41:26 +0800

    Add portfolio color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        apercu: ['var(--font-apercu)', 'sans-serif'],
+        apercu: ['var(--fohnt-apercu)', 'sans-serif'],
         canela: ['var(--font-canela)', 'sans-serif'],
-      }
+      },
+      colors: {
+        primary: '#101110',
+        secondary: '#E42F2F',
+        tertiary: '#F3994C',
+        highlight: '#F1F1EA',
+      },
     },
   },
   plugins: [],
-}
+};
