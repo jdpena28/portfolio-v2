710e2207e7ee0d02fd5eaf70162e0dd7ef08860b
jdpena28 <johnhenrich1128@gmail.com>
2023-06-22 22:12:40 +0800

    CHORE: add section for about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import AboutMe from '@/components/sections/AboutMe';
 import Home from '@/components/sections/Home';
 
 export default function Index() {
   return (
     <main className="container mx-auto w-full px-5">
       <Home />
+      <AboutMe />
     </main>
   );
 }
