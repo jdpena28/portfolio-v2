86d371a17cf6f84494608929244846d9cbdaa928
jdpena28 <johnhenrich1128@gmail.com>
2023-06-24 22:15:51 +0800

    CHORE: add default SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-'use client';
-
 import AboutMe from '@/components/sections/AboutMe';
 import Home from '@/components/sections/Home';
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Portfolio | John Henrich Dela Pena',
+  description:
+    'Full stack developer with several months experience in Full Stack Javascript Application mainly focus on web and mobile application. Lean with working on Next.js Framework. React.js Typescript Enthusiast. Keeping up with the latest emerging technology.',
+};
 
 export default function Index() {
   return (
