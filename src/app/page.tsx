9b8cf7c40464cfdfefc5125e9f35cc95139c2b82
jdpena28 <johnhenrich1128@gmail.com>
2023-08-03 20:55:23 +0800

    Add particles as BG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import AboutMe from '@/components/sections/AboutMe';
 import Contact from '@/components/sections/Contact';
 import Home from '@/components/sections/Home';
 import Projects from '@/components/sections/Projects';
+import BGParticles from '@/components/Particles';
 
 export default function Index() {
   return (
-    <main className="mx-auto w-full px-5">
+    <main className="relative w-full px-5">
       <Home />
       <AboutMe />
       <Projects />
       <Contact />
+      <BGParticles />
     </main>
   );
 }
